404f7d10164e13016ac0d26a6792ec2fb4937e5b
Sakib Niaz <sakibniaz221@gmail.com>
2025-04-22 18:10:25 -0400

    added to do list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,18 @@ import './App.css';
 function App() {
   return (
     <>
-    
         <div className="center">
           <h1 className="header">PerfectStudy</h1>
-          
+          <h2>TO DO: PAGES</h2>
+          <ul>
+            <il>Home Landing Page</il>
+            <il>Card Creation Page</il>
+            <il>QR Code page</il>
+            <il>FlashCard prompt page</il>
+            <il>FlashCard answer page</il>
+          </ul>
         </div>
     </>
-    
   );
 }
 
