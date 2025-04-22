856a89260e2aa9cb9c9d7988ebc663745fc48037
nahomwondimu <nahomawt@gmail.com>
2025-04-21 23:32:29 -0400

    developed the django db

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+# in django, anything you want to store in the database is put together here in the form of a python class.
+# ex, if I want to save teddy bear info, I would make a class with the attributes I want.
